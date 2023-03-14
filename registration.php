134210f30d0a03b54cf25c212af89fae9a478bc2
davearyan81 <davearyan81@gmail.com>
2023-03-14 18:00:54 +0530

    added

--- registration.php
+++ registration.php
@@ -1,4 +1,5 @@
 <?php
+session_start();    
 include("includes/header.php");
 ?>
 <div class="wrapper m-auto">
@@ -16,37 +17,42 @@ include("includes/header.php");
             <input type="submit" class="account" value="Have an Account?">
         </div>
     </div>
-    <form class="row  needs-validation form-right m-auto" novalidate>
-
-        <div class="col-md-6 ">
-            <label for="validationCustom01" class="form-label"></label>
-            <input type="text" class="form-control" placeholder="First Name" id="validationCustom01" value="Mark"
-                required>
+    <form class="row  needs-validation form-right m-auto" action="registrationcode.php" method="post" novalidate>
+<?php include("message.php"); ?>
+        <div class="col-md-6">
+            <label for="validationCustom01" class="form-label">First name</label>
+            <input type="text" class="form-control" id="validationCustom01" name="txtfname" required>
+            
+        </div>
+        <div class="col-md-6">
+            <label for="validationCustom02" class="form-label">Last name</label>
+            <input type="text" class="form-control" id="validationCustom02" name="txtlname" required>
+            
+        </div>
 
         <div class="col-md-12">
-            <label for="validationCustom03" class="form-label"></label>
-            <input type="email" class="form-control mb-4" placeholder="E-mail" id="validationCustom03" required>
-
+            <label for="validationCustom03" class="form-label">E-mail</label>
+            <input type="email" class="form-control mb-4" id="validationCustom03" name="txtemail" required>
+           
         </div>
         <div class="col-md-6">
             <label for="validationCustom03" class="form-label"></label>
-            <input type="password" class="form-control mb-4" placeholder="Password" id="validationCustom03" required>
-
+            <input type="password" class="form-control mb-4" placeholder="Password" name="txtpass" id="validationCustom03" required>
+           
         </div>
         <div class="col-md-6">
-            <label for="validationCustom03" class="form-label"></label>
-            <input type="password" class="form-control mb-4" placeholder="Confirm Password" id="validationCustom03"
-                required>
-
+            <label for="validationCustom03" class="form-label">Confirm Password </label>
+            <input type="password" class="form-control mb-4" name="txtcpass" id="validationCustom03" required>
+           
         </div>
         <div class="col-md-12 mb-2">
             <label for="validationCustom05" class="form-label">Phone no</label>
-            <input type="tel" class="form-control mb-4" id="validationCustom05" required>
-
+            <input type="tel" class="form-control mb-4" id="validationCustom05" name="txtphno" required>
+            
         </div>
         <div class="col-12 form-field">
 
-            <button class="btn btn-primary" type="submit">Submit form</button>
+            <button class="btn btn-primary" type="submit" name="btnsubmit">Submit form</button>
         </div>
 
     </form>
--- registrationcode.php
+++ registrationcode.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include("admin/config/dbcon.php");
 if (isset($_POST['btnsubmit'])) {
     $fname = $_POST['txtfname'];
