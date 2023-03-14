95a45720d31c1f0c0fe8031ab59c593d5639e1fa
davearyan81 <davearyan81@gmail.com>
2023-03-14 17:49:30 +0530

    hello

--- registration.php
+++ registration.php
@@ -17,7 +17,7 @@ include("includes/header.php");
         </div>
     </div>
     <form class="row  needs-validation form-right m-auto" action="registrationcode.php" method="post" novalidate>
-<?php include("message.php");?>
+<?php include("message.php"); ?>
         <div class="col-md-6">
             <label for="validationCustom01" class="form-label">First name</label>
             <input type="text" class="form-control" id="validationCustom01" name="txtfname" required>
@@ -33,15 +33,7 @@ include("includes/header.php");
             </div>
         </div>
 
-        </div>
-        <div class="col-md-6">
-            <label for="validationCustom02" class="form-label"></label>
-            <input type="text" class="form-control" placeholder="Last Name" id="validationCustom02" value="Otto"
-                required>
-
-        </div>
         <div class="col-md-12">
-<<<<<<< HEAD
             <label for="validationCustom03" class="form-label">E-mail</label>
             <input type="email" class="form-control mb-4" id="validationCustom03" name="txtemail" required>
             <div class="invalid-feedback">
@@ -68,27 +60,6 @@ include("includes/header.php");
             <div class="invalid-feedback">
                 Please provide a valid zip.
             </div>
-=======
-            <label for="validationCustom03" class="form-label"></label>
-            <input type="email" class="form-control mb-4" placeholder="E-mail" id="validationCustom03" required>
-
-        </div>
-        <div class="col-md-6">
-            <label for="validationCustom03" class="form-label"></label>
-            <input type="password" class="form-control mb-4" placeholder="Password" id="validationCustom03" required>
-
-        </div>
-        <div class="col-md-6">
-            <label for="validationCustom03" class="form-label"></label>
-            <input type="password" class="form-control mb-4" placeholder="Confirm Password" id="validationCustom03"
-                required>
-
-        </div>
-        <div class="col-md-12 mb-2">
-            <label for="validationCustom05" class="form-label">Phone no</label>
-            <input type="tel" class="form-control mb-4" id="validationCustom05" required>
-
->>>>>>> d1e1bd39b1cf26557995cccd8ccc595f60b25591
         </div>
         <div class="col-12 form-field">
 
